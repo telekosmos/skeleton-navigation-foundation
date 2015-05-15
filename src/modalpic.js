10efac9e25cadf152c0ebd26de0dc1b458edc6d1
telekosmos <telekosmos@ymail.com>
2015-05-15 19:18:49 +0200

    Bumped to skeleton navigation to 0.13.1

--- src/modalpic.js
+++ src/modalpic.js
@@ -11,15 +11,11 @@ export class ModalPic {
   }
   
   attached() {
-    // var _this = this;
-    let self = this;
+
     this._aElem = this.element.getElementsByTagName('a')[0];
     this._aElem.addEventListener('click', (ev) => {
       // Chrome debugger fails to show the actual value of this when using =>
-
-      // this = this._aElem in here!!!
-      // let modalId = this.getAttribute('data-reveal-id'); // ev.target.getAttribute('data-reveal-id');
-      let one = this; // this here is compiled to _this
+      
       let _aElem = this._aElem; // self._aElem;
 
       let modalId = _aElem.attributes['data-reveal-id']; // this.attributes['data-reveal-id'].value;
